3f9800e6a3400e780dc52223d437691aaf4c2f7a
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2024-12-03 10:21:21 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/odin-cv-application_v2/',
+  base: '/odin-memory-card/',
   plugins: [react()],
 })
