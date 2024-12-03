e0592869f38a765e3dae44fb09e6034c3db95e63
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2024-12-03 10:14:37 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="container">
+        <div id="top">
+          <div><h1>Memory Game</h1></div>
+          <div>
+            <h2>Score</h2>
+            <h2>Top Score</h2>
+          </div>
+        </div>
+        <div id="body">Body</div>
+      </div>
+    </>
+  )
+}
+
+export default App
