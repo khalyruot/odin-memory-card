80714a1c4c0e91b817e29fc1458c426464aec930
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2024-12-05 09:26:02 +0700

    pick only odd pokemon

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -4,24 +4,42 @@ const FetchData = () => {
     const[dataImage, setDataImage] = useState([]);
 
     useEffect(()=>{
-        fetch('https://pokeapi.co/api/v2/pokemon/?limit=12')
-        .then(response => response.json())
-        .then(data => {
-          let results = data.results;
-          let promisesArray = results.map(result=>{
-            return fetch(result.url).then(response=>response.json());
-          })
-          return Promise.all(promisesArray);
-        })
-        .then((pokemonData)=>{
-          setDataImage(pokemonData);
-          console.log("Main Pokemon State: ", pokemonData)})
-        })
+        const fetchData = async () => {
+          try{
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=24');
+            const data = await response.json();
+            let results = data.results;
+
+            let promisesArray = results.map(result=>fetch(result.url).then(res=>res.json()));
+            const pokemonData = await Promise.all(promisesArray);
+
+            setDataImage(pokemonData);
+            
+           
+        
+          }
+          catch(error){
+            console.log("Error Fetching Data: ", error);
+          }
+        };
+        fetchData();
+        },[]);
+
+  let oddDataImage = [];
+
+  function displayRandomDataImage(dataImage){
+    let oddImageInsideFunc = dataImage.filter((_, index) => index % 2 !== 0);
+    oddDataImage = oddImageInsideFunc;
+    
+  }
+  displayRandomDataImage(dataImage);
+  
+
   return (
 
     <div >
       <div>
-          <ul id="image_grid">{dataImage.map((list, index)=>(
+          <ul id="image_grid">{oddDataImage.map((list, index)=>(
             <li key={index}>
               {list.id} | {list.name}
               <img src={list.sprites.front_default} alt={list.name} width="100" />
