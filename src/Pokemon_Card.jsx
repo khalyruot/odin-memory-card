81ceef4524608f166951ea79e592454f9eb02415
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2024-12-24 10:45:39 +0700

    add css style grid

--- src/Pokemon_Card.jsx
+++ src/Pokemon_Card.jsx
@@ -1,9 +1,8 @@
-export default function PokemonCard({id, name, src, onClick}){
+export default function PokemonCard({name, src, onClick}){
     return(
         <div onClick={onClick}>
             <img src={src} alt={name} />
             <h3>{name}</h3>
-            <p>{id}</p>
         </div>
     )
 }
